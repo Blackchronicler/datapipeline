7378c647a41126ec163909e21565ebef5eea5ca0
thierryasongwe0000 <thierry.asongwe@otto.de>
2022-09-19 22:26:44 +0200

    TA| git crawler build progress

--- git_crawler.py
+++ git_crawler.py
@@ -2,6 +2,7 @@ from secrets import token_bytes
 import requests
 from pprint import pprint
 from github import Github
+import pandas as pd
 
 class Git_Crawler:
         
@@ -13,37 +14,30 @@ class Git_Crawler:
         #print(user, pwd_token, sep="")
 
     # Instantiating GitHub
-    g = Github(login_or_token= user_token, password= pwd_token)
+    #g = Github(login_or_token= user_token, password= pwd_token)
+    g = Github()
 
-    def __init__(self, git_entity) -> None:
-            self.git_entity = git_entity
+    def __init__(self, git_entity : str) -> None:
+        self.git_entity = git_entity
+            
 
     def _getting_user(self):
         """ Extracting the necessary user information from GitHub """
         
         try:
             user = self.g.get_user(self.git_entity)
-            print(f"The User\'s name is: {user.name}")
+            return (f"The User\'s name is: {user.name}")
 
         except Exception as e:
-            print(f"We have the following problem with \"User Name\": {str(e)}")
-            
-    def _getting_organisation_details(self):
-        """
-        Extracting the necessary organisational data 
-        (languages, repositories and more) from GitHub
+            return f"We have the following problem with \"User Name\": {str(e)}"
         
-        """
         
-        try:       
-            languages_used = {}
+    def _getting_languages_used(self):
+        """ Getting all programming languages being used in the organisation """
+        try:
             orga = self.g.get_organization(self.git_entity)
-            #print(f"The following Organisation exists: {orga.login}")
-            members = list(orga.get_members(filter_="all"))
-            print(f"The Organisation has: {len(members)} members.")
-            repos = list(orga.get_repos(type= "all", sort= "full_name"))
-            #print(f"The Organisation has: {len(repos)} repositories.")
-            
+            repos = list(orga.get_repos(type="all", sort="full_name"))
+            languages_used = {}
             for repo in repos[:5]:
                 langs_used = repo.get_languages()
                 for language in langs_used:
@@ -51,12 +45,38 @@ class Git_Crawler:
                         languages_used[language] = langs_used[language] 
                     else:
                         languages_used[language] = (languages_used[language] + langs_used[language])
-            pprint(languages_used) 
+            df = pd.DataFrame(list(languages_used.items()), columns=["languages", "bytes"])
+            return df
+        
+        except Exception as e:
+            return f'We have the following problem with \"Organisation Languages\": {str(e)}'
+        
+            
+    def _getting_organisation_details(self):
+        """
+        Extracting the necessary organisational data 
+        (languages, repositories and more) from GitHub
+        
+        """
+        try:       
+            orga = self.g.get_organization(self.git_entity)
+            orga_name = orga.login
+            members = list(orga.get_members(filter_="all"))
+            repos = list(orga.get_repos(type= "all", sort= "full_name"))
+            data_collected = {
+                "organisation" : [orga_name],
+                "number of members" : [len(members)],
+                "number of repositories" : [len(repos)],
+                "number of languages" : [len(self._getting_languages_used())]
+                }
+            df = pd.DataFrame(data_collected)
+            return df 
                 
         except Exception as e:
-            print(f'We have the following problem with \"Organisation Name\": {str(e)}')
+            return f'We have the following problem with \"Organisation Details\": {str(e)}'
+        
 
 
-if __name__ == "__main__":
-    Git_Crawler("blackchronicler")._getting_user()
-    Git_Crawler("OSGeo")._getting_organisation_details()
+#Git_Crawler("blackchronicler")._getting_user()
+sample = Git_Crawler("OSGeo")._getting_languages_used()
+print(sample)
