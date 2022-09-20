e793ee46f7b01ed4f230dc1809a1326aed5e99e8
Sadaf Ghasemi <sadaf.ghasemi@otto.de>
2022-09-20 10:34:34 +0200

    Merge ty br to main and move main to sadaf br

---
+++ db.py
@@ -0,0 +1,26 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="postgres",
+    user="postgres",
+    password="postgres")
+
+cur = conn.cursor()
+
+# cur.execute("INSERT INTO public.organizations(org_name, repos, members)VALUES (%s, %s, %s);",
+#            ("test", 143, 760))
+# cur.execute("INSERT INTO public.organizations(org_name, repos, members)VALUES (%s, %s, %s);",
+#            ("test1", 143, 760))
+# cur.execute("INSERT INTO public.organizations(org_name, repos, members)VALUES (%s, %s, %s);",
+#            ("test2", 143, 760))
+# conn.commit()
+
+cur.execute("SELECT * FROM public.organizations")
+rows = cur.fetchall()
+
+for r in rows:
+    print(r)
+
+cur.close()
+conn.close()
