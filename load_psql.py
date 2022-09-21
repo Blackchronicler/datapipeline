f995a317434b36d7c8aff8ebd7c8dbc859812f8f
thierryasongwe0000 <thierry.asongwe@otto.de>
2022-09-21 17:46:33 +0200

    TA | recent file updates

--- load_psql.py
+++ load_psql.py
@@ -2,12 +2,14 @@
 import psycopg2
 from git_crawler import Git_Crawler as gc
 
-#Postgres details
+#Connection details
 hostname = "localhost"
 database = "development"
 username = gc.access_tokens[2]
 pwd = gc.access_tokens[3]
 port_id = 5432
+cur = None
+conn = None
 
 try:
     conn= psycopg2.connect(
@@ -16,8 +18,16 @@ try:
         user = username,
         password = pwd,
         port = port_id)
-
-    conn.close()
     
+    cur = conn.cursor()
+    
+    create_organisation
+
 except Exception as e:
     print (f"The connection to the database has the following problem: {str(e)}")
+    
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
