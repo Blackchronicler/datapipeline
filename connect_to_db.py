7687ab059925d40baa79c33e89cedabea964dec7
Black Chronicler <69725403+Blackchronicler@users.noreply.github.com>
2022-09-27 15:27:12 +0200

    Update connect_to_db.py

--- connect_to_db.py
+++ connect_to_db.py
@@ -1,33 +1,37 @@
 # Importing necessary libraries
 import psycopg2
 
-
 class ConnectToDatabase:
-
+    
     def __init__(self) -> None:
         pass
 
     def _connecting_to_db():
         """ Connect to the PostgreSQL database server """
 
+        # # Getting PAT for auth @ Database
+        # with open("pat.txt", "r") as f:
+        #     access_tokens = f.readlines()
+        #     user_token = access_tokens[2]
+        #     pwd_token = access_tokens[3]
+        
         # Connection details
         db_parameters = {
-            "host": "localhost",
-            "database": "GithubCrawler",
-            "user": "postgres",
-            "password": "postgres",
-            "port": 5432}
-
+        "host" : "localhost",
+        "dbname" : "wsl",
+        "user" : "thierry",
+        "password" : "admin",
+        "port" : 5432,
+        "options" : "-c search_path=dbo,github"
+        }
+        
         try:
             print("\n", "Connecting to the PostgreSQL database...Please wait...", "\n", sep="")
-            conn = psycopg2.connect(**db_parameters)
-            print("Connection successful :) ")
+            conn= psycopg2.connect(**db_parameters)
+            print("Connection successful :) ", "\n", sep="")
             return conn
 
         except Exception as e:
             print(f"Connection to database was unsuccessful because: {str(e)}")
-            # exit(1)
-
-
-if __name__ == "__main__":
-    ConnectToDatabase._connecting_to_db()
+            exit(1)
+            
