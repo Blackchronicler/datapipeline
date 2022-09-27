1f9a1eda7d54439a6489fe12e1963647bc1d0105
Black Chronicler <69725403+Blackchronicler@users.noreply.github.com>
2022-09-27 15:26:14 +0200

    Update create_tables_db.py

--- create_tables_db.py
+++ create_tables_db.py
@@ -1,70 +1,74 @@
 # Importing necessary libraries
-from connect_to_db import ConnectToDatabase
+#from packages.connect_to_db import ConnectToDatabase
 
 #  Tables and queries for database
 database_tables = ["org_langs", "languages", "organisation"]
 create_queries = {
-    "create_organisation_table": """ CREATE TABLE IF NOT EXISTS organisation (
+"create_organisation_table" : """ CREATE TABLE IF NOT EXISTS organisation (
                                 org_id int NOT NULL PRIMARY KEY,
                                 organisation_name varchar(100) NOT NULL,
                                 number_of_members int NOT NULL,
                                 number_of_repositories int NOT NULL,
                                 number_of_languages int NOT NULL); """,
 
-    "create_language_table": """ CREATE TABLE IF NOT EXISTS languages (
+"create_language_table" : """ CREATE TABLE IF NOT EXISTS languages (
                                     lang_id int NOT NULL PRIMARY KEY,
                                     language_typ varchar(100) NOT NULL,
                                     number_of_bytes int NOT NULL,
                                     organisation_name varchar(100) NOT NULL); """,
-
-    "create_org_lang_table": """ CREATE TABLE IF NOT EXISTS org_langs (
+                                    
+"create_org_lang_table" : """ CREATE TABLE IF NOT EXISTS org_langs (
                                     org_langs_id int NOT NULL PRIMARY KEY,
                                     organisation_id int NOT NULL REFERENCES organisation(org_id),
                                     language_typ_id int NOT NULL REFERENCES languages(lang_id)); """
 }
 
-
 def delete_tables_db(conn):
     """ Delete all tables in database """
-
+    
     cur = conn.cursor()
-    ## TODO check existing table
     try:
         for table in database_tables:
             cur.execute(f"DROP TABLE IF EXISTS {table};")
-        conn.commit()
-        print("All tables have been deleted from the database!!")
-
+        conn.commit()    
+        print("All tables have been deleted from the database!!", "\n", sep="")
+    
     except Exception as e:
         print(f"Error while deleting table(s): {str(e)}")
         conn.rollback()
-
+        return 1
+    
     cur.close()
-
 
 def create_tables_db(conn):
     """ Create tables in the database """
-
+    
     cur = conn.cursor()
     try:
         for query in create_queries:
             cur.execute(create_queries[query])
         conn.commit()
-        print("Tables successfully created in the database.")
-
+        print("Table(s) successfully created in the database!! :D", "\n", sep="")
+        
     except Exception as e:
         print(f"Error while creating table(s): {str(e)}")
         conn.rollback()
-
+        return 1
+    
     cur.close()
-
-
-def main(con):
+    
+def main(conn):
     """ Executes the above """
-    delete_tables_db(con)
-    create_tables_db(con)
-
+    
+    try:
+        delete_tables_db(conn)
+        create_tables_db(conn)
+        conn.close()
+        
+    except Exception as e:
+        print(f"Error while running the main method in create_tables_db file: {str(e)}")
+        exit(1)
+    
 
 if __name__ == "__main__":
-    conn = ConnectToDatabase._connecting_to_db()
-    main(conn)
+    main()
