8e4bce8af77df531867afccc8072b264bb58ce9f
Black Chronicler <69725403+Blackchronicler@users.noreply.github.com>
2022-09-27 15:18:24 +0200

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -1,41 +1,39 @@
-from connect_to_db import ConnectToDatabase as cd
-
+from packages.connect_to_db import ConnectToDatabase as cd
 
 class FetchData:
-
-    def __init__(self, conn, db_table: str) -> None:
+    
+    def __init__(self, db_table : str) -> None:
         self.conn = conn
         self.db_table = db_table
 
     def _fetch_data(self):
         """ Check data availability in the DB """
-
+        
         sample_db = 0
-        cur = self.conn.cursor()
+        conn = cd._connecting_to_db()
+        cur = conn.cursor()
         try:
             cur.execute(f"SELECT * FROM {self.db_table};")
             sample_db = cur.fetchall()
-            self.conn.commit()
-            return sample_db
-
+            conn.commit()
+            return  sample_db
+        
         except Exception as e:
             print(f"Error while fetching data: {str(e)}")
-            self.conn.rollback()
-
+            conn.rollback()
+            return 1
+        
         cur.close()
-        self.conn.close()
+        conn.close()
 
     def _show_results(self):
         """ Preview into the data from the DB """
-
+        
         try:
             results = self._fetch_data()
-            for record in results[:10]:  # Adjust results limit according to needs
+            for record in results[:10]:     # Adjust results limit according to needs
                 print(record)
-
+                
         except Exception as e:
             print(f" Preview unavailable due to this error: {str(e)}")
-
-
-if __name__ == "__main__":
-    FetchData(cd._connecting_to_db(), "organisation")._show_results()
+            exit(1)
